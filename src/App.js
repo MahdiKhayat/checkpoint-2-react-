7427d012e9b8fdbe4043078e64b38dd9b4a34ff5
mahdi <mahdikhayat20@gmail.com>
2022-03-07 19:32:33 +0100

    2nd checkpoint

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
+import Form from 'react-bootstrap/Form';
+import Button from 'react-bootstrap/Button';
+import Stack from 'react-bootstrap/Stack';
+import { Col , Row} from "react-bootstrap";
 
 function App() {
   return (
@@ -7,7 +11,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to my first react project !!!
         </p>
         <a
           className="App-link"
@@ -15,8 +19,40 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          
         </a>
+
+        <Form>
+          <Form.Group as={Row} className="mb-3" controlId="formPlaintextEmail">
+            <Form.Label column sm="2">
+              Email
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control plaintext readOnly defaultValue="email@example.com" />
+            </Col>
+          </Form.Group>
+
+          <Form.Group as={Row} className="mb-3" controlId="formPlaintextPassword">
+            <Form.Label column sm="2">
+              Password
+            </Form.Label>
+            <Col sm="10">
+              <Form.Control type="password" placeholder="Password" />
+            </Col>
+          </Form.Group>
+        </Form>
+
+
+          <Stack gap={2} className="col-md-5 mx-auto">
+            <Button variant="secondary">Save changes</Button>
+            <Button variant="outline-secondary">Cancel</Button>
+          </Stack>
+
+          <>
+            <Form.Label>Are you happy for my effort ?</Form.Label>
+            <Form.Range />
+          </>
+
       </header>
     </div>
   );
